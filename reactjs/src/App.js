00519fd3147ab07ec9e09d575f09fe5c2703cd77
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-09-07 13:07:50 -0300

    update basic code

---
+++ reactjs/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+  rafae
+    </>
+  );
+}
+
+export default App;
---
+++ reactjs/src/reducers/index.js
@@ -0,0 +1,5 @@
+// import { clickReducer } from './clickReducer';
+// import { combineReducers } from 'redux';
+// export const Reducers = combineReducers({
+//     clickState: clickReducer,
+// });
