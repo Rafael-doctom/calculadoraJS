a3aa5b3ed3095994b534e5b52bc7cc08a41540e6
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-09-07 13:31:52 -0300

    basic calculator & styles add

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -3,7 +3,69 @@ import './App.css';
 function App() {
   return (
     <>
-  rafae
+      <div className="container">
+        <div className="content">
+          <div className="screen-calculator">
+            <div className="result">
+              0000
+            </div>
+            <div className="calculator">
+              <button>
+                +
+              </button>
+              <button>
+                -
+              </button>
+              <button>
+                x
+              </button>
+              <button>
+                /
+              </button>
+            </div>
+            <div className="calculator">
+              <button>
+                9
+              </button>
+              <button>
+                8
+              </button>
+              <button>
+                7
+              </button>
+              <button>
+                6
+              </button>
+            </div>
+            <div className="calculator">
+              <button>
+                5
+              </button>
+              <button>
+                4
+              </button>
+              <button>
+                3
+              </button>
+              <button>
+                2
+              </button>
+            </div>
+            <div className="calculator">
+              <button>
+                1
+              </button>
+              <button>
+                AC
+              </button>
+              <button className='equal'>
+                =
+              </button>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
