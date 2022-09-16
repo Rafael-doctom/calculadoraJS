51155beec6512328e3fcb837b80a6f294b7a9c54
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-09-16 11:04:44 -0300

    update

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import { Provider } from 'react-redux';
-import { Store } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <Provider store={Store}>
   <App />
-  // </Provider>
 );
 
